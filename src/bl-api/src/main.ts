f6dfa0b5f1b460e1b92aa274a3504c48af8bfd05
RicFg04 <127856535+RicFg04@users.noreply.github.com>
2024-02-13 23:49:30 +0000

    Update main.ts

--- src/bl-api/src/main.ts
+++ src/bl-api/src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-  }));
+  app.use(cors());
   await app.listen(process.env.PORT);
 }
 bootstrap();
